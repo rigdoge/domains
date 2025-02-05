8f0831bce30d4a2343ae20919171908491f3cf82
rigdoge <rigdoge@users.noreply.github.com>
2025-02-05 17:17:47 +0800

    fix: service worker and PWA configuration

--- next.config.js
+++ next.config.js
@@ -2,7 +2,21 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/domains\.facesome\.com\/_next\/.*/i,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'next-static',
+        expiration: {
+          maxEntries: 64,
+          maxAgeSeconds: 24 * 60 * 60 // 24 hours
+        }
+      }
+    }
+  ],
+  buildExcludes: [/app-build-manifest.json$/]
 });
 
 /** @type {import('next').NextConfig} */
